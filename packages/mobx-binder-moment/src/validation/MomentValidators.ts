8758a0492a925d25c1e654f28909668759709e44
Carsten Rohrbach <carsten@r0hrbach.de>
2019-05-08 15:52:45 +0200

    Fix failing MomentValidator unit tests

--- packages/mobx-binder-moment/src/validation/MomentValidators.ts
+++ packages/mobx-binder-moment/src/validation/MomentValidators.ts
@@ -2,22 +2,26 @@ import * as moment from 'moment'
 import { BinderValidationResult, BinderValidator } from 'mobx-binder'
 
 export class MomentValidators {
+
     public static dayInFuture(messageKey = 'validations.dayInFuture'): BinderValidator<moment.Moment> {
-        return (value) => MomentValidators.validate(moment.isMoment(value) && value.diff(moment(), 'days') <= 0, messageKey, { value })
+        return (value) => MomentValidators.validate(
+            moment.isMoment(value) && value.diff(MomentValidators.today(), 'days') <= 0, messageKey, { value })
     }
 
     public static todayOrInFuture(messageKey = 'validations.todayOrInFuture'): BinderValidator<moment.Moment> {
-        return (value) => MomentValidators.validate(moment.isMoment(value) && value.diff(moment(), 'days') < 0, messageKey, { value })
+        return (value) => MomentValidators.validate(
+            moment.isMoment(value) && value.diff(MomentValidators.today(), 'days') < 0, messageKey, { value })
     }
 
     public static dayInPast(messageKey = 'validations.dayInPast'): BinderValidator<moment.Moment> {
-        return (value) => MomentValidators.validate(moment.isMoment(value) && value.diff(moment(), 'days') >= 0, messageKey, { value })
+        return (value) => MomentValidators.validate(
+            moment.isMoment(value) && value.diff(MomentValidators.today(), 'days') >= 0, messageKey, { value })
     }
 
     public static age(minAge?: number, maxAge?: number, messageKey = 'validations.age'): BinderValidator<moment.Moment> {
         return (value) => {
             if (moment.isMoment(value)) {
-                const theAge = moment().diff(value, 'years')
+                const theAge = MomentValidators.today().diff(value, 'years')
                 return MomentValidators.validate(
                     (!!minAge && theAge < minAge) || (!!maxAge && theAge > maxAge),
                     messageKey,
@@ -28,10 +32,13 @@ export class MomentValidators {
     }
 
     public static todayOrInPast(messageKey = 'validations.todayOrInPast'): BinderValidator<moment.Moment> {
-        return (value) => MomentValidators.validate(moment.isMoment(value) && value.diff(moment(), 'days') > 0, messageKey, { value })
+        return (value) => MomentValidators.validate(
+            moment.isMoment(value) && value.diff(MomentValidators.today(), 'days') > 0, messageKey, { value })
     }
 
     private static validate(errorCondition: boolean, message: string, args?: any): BinderValidationResult {
         return errorCondition ? { messageKey: message, args } : {}
     }
+
+    private static today = () => moment()
 }
--- packages/mobx-binder-moment/src/validation/MomentValidatorsSpec.ts
+++ packages/mobx-binder-moment/src/validation/MomentValidatorsSpec.ts
@@ -5,14 +5,16 @@ import * as moment from 'moment'
 import data_driven = require('data-driven')
 import sinon = require('sinon')
 
-describe('Validators', () => {
+describe('MomentValidators', () => {
     const sandbox = sinon.createSandbox()
     let clock: sinon.SinonFakeTimers
 
     const date = (str: string) => moment(str, 'DD.MM.YYYY')
+    const today = date('12.03.2019')
 
     beforeEach(() => {
         clock = sandbox.useFakeTimers(new Date(2019, 3, 12).getTime())
+        sandbox.stub(MomentValidators as any, 'today').returns(today)
     })
 
     afterEach(() => {
@@ -22,10 +24,10 @@ describe('Validators', () => {
     data_driven([
         { method: 'dayInFuture', args: [], value: date('01.01.2099') },
         { method: 'todayOrInFuture', args: [], value: date('01.01.2099') },
-        { method: 'todayOrInFuture', args: [], value: moment() },
+        { method: 'todayOrInFuture', args: [], value: today },
         { method: 'dayInPast', args: [], value: date('01.01.2018') },
         { method: 'todayOrInPast', args: [], value: date('01.01.2018') },
-        { method: 'todayOrInPast', args: [], value: moment() },
+        { method: 'todayOrInPast', args: [], value: today },
         { method: 'age', args: [ 18 ], value: date('01.01.2001') },
         { method: 'age', args: [ undefined, 18 ], value: date('01.01.2002') },
         { method: 'age', args: [ 18, 99 ], value: date('01.01.1920') },
@@ -39,10 +41,10 @@ describe('Validators', () => {
 
     data_driven([
         { method: 'dayInFuture', args: [], value: date('01.01.2018'), messageArgs: {} },
-        { method: 'dayInFuture', args: [], value: moment() },
-        { method: 'todayOrInFuture', args: [], value: date('11.04.2019') },
+        { method: 'dayInFuture', args: [], value: today },
+        { method: 'todayOrInFuture', args: [], value: date('11.02.2019') },
         { method: 'dayInPast', args: [], value: date('13.04.2019') },
-        { method: 'dayInPast', args: [], value: moment() },
+        { method: 'dayInPast', args: [], value: today },
         { method: 'todayOrInPast', args: [], value: date('13.04.2019') },
         { method: 'age', args: [ 18 ], value: date('01.01.2002'), messageArgs: { minAge: 18, maxAge: undefined } },
         { method: 'age', args: [ undefined, 99 ], value: date('01.01.1919'), messageArgs: { minAge: undefined, maxAge: 99 } },
