de88c38a7a32336945397afd601f60dbc7a5646b
Carsten Rohrbach <carsten.rohrbach@codecentric.de>
2021-01-08 17:04:55 +0100

    Move documentation to docz (wip)

---
+++ packages/docs/doczrc.js
@@ -0,0 +1,24 @@
+export default {
+    typescript: true,
+    menu: [
+        'Overview',
+        'Getting Started',
+        'Basic API',
+        {
+            name: 'Usage',
+            menu: [
+                'Prerequisites',
+                'Preparing a Binder',
+                'Loading and storing field values',
+                'Validation',
+                'Conversion',
+                'Conditional Visibility',
+                'Change Events',
+                'Submission',
+                'Rendering',
+            ]
+        },
+        'TODOs',
+        'Development'
+    ]
+}
