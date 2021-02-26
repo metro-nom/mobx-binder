81fa0cd73bc6301743c5e7362198bc0ba2837305
Carsten Rohrbach <carsten.rohrbach@codecentric.de>
2021-02-26 13:58:41 +0100

    Be more strict in FieldStore property changes

--- packages/mobx-binder-core/src/model/fields/FieldStore.ts
+++ packages/mobx-binder-core/src/model/fields/FieldStore.ts
@@ -21,9 +21,9 @@ export interface FieldStore<ValueType> {
     showValidationResults: boolean
 
     /**
-     * The current value of the form field.
+     * The current value of the form field. To be updated on frontend side via `updateValue()`
      */
-    value: ValueType
+    readonly value: ValueType
 
     /**
      * The validity status of a form field. Always set, except for unfinished async validations.
@@ -34,7 +34,7 @@ export interface FieldStore<ValueType> {
     /**
      * Set to `true` when a field gets focus for the first time.
      */
-    visited: boolean
+    readonly visited: boolean
 
     /**
      * Set to `true` on first change. Fields are untouched initially, after load() or reset()
@@ -49,6 +49,7 @@ export interface FieldStore<ValueType> {
 
     /**
      * If `valid === false`, containing the validation message.
+     * You can set a custom error message here, in which case the field gets invalid.
      * This property is replaced by a getter/setter on bind.
      */
     errorMessage?: string
@@ -62,7 +63,7 @@ export interface FieldStore<ValueType> {
      * Indicates if the field is a mandatory field.
      * This property is replaced by a getter on bind.
      */
-    required: boolean
+    readonly required: boolean
 
     /**
      * This function must be used to update field values via the frontend.
