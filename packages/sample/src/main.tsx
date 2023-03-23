22f61f8cf959b37421c694c2d130b832ce09e81e
Carsten Rohrbach <carsten.rohrbach@codecentric.de>
2023-03-23 12:43:58 +0100

    Fix sample

--- packages/sample/src/main.tsx
+++ packages/sample/src/main.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'mobx-react'
 import { App } from 'app'
-import { stores } from './stores'
 
 // tslint:disable no-submodule-imports
 import 'bootstrap/dist/css/bootstrap.css'
 
 // render react DOM
-ReactDOM.render(
-    <Provider {...stores}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
